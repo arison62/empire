0fa8276ed815ed241299bb6395985f5aba1c44e0
Albert Fotso <legrandpone1@gmail.com>
2025-03-03 20:37:58 +0100

    Drawer animation ok

--- src/main.js
+++ src/main.js
@@ -1,34 +1,12 @@
 import './style.css'
 import { createIcons, icons } from 'lucide';
 createIcons({icons});
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('cards-container');
-    const template = document.getElementById('card-template');
-    const numCards = 10;
-    const colors = [
-      'from-cyan-200 to-blue-300',
-      'from-green-200 to-teal-300',
-      'from-lime-200 to-emerald-300',
-      // ... ajouter les autres couleurs
-    ];
+document.getElementById('btn-menu-open').addEventListener('click', function() {
+  this.closest('.group').classList.toggle('is-open');
   
-    // Générer les cartes
-    for (let i = 0; i < numCards * 2; i++) {
-      const clone = template.content.cloneNode(true);
-      const card = clone.querySelector('.carousel-card');
-      card.querySelector('div').className = `h-full w-full rounded-lg bg-gradient-to-br ${colors[i % numCards]}`;
-      container.appendChild(clone);
-    }
+});
+
+document.getElementById('btn-menu-close').addEventListener('click', function() {
+  this.closest('.group').classList.toggle('is-open');
   
-    // Gestion de l'hover
-    const carousel = document.getElementById('carousel');
-    carousel.querySelectorAll('.carousel-card').forEach(card => {
-      card.addEventListener('mouseenter', () => {
-        carousel.style.animationPlayState = 'paused';
-      });
-      
-      card.addEventListener('mouseleave', () => {
-        carousel.style.animationPlayState = 'running';
-      });
-    });
-  });
+});
